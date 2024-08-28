5961f79d01e8c52286bd47f79359ce61c0c5d335
Victoria Lundberg <victoria.lundbrg@gmail.com>
2024-08-28 10:40:01 +0200

    🎨 Display of animals in separate component

--- src/components/AnimalsDisplay.tsx
+++ src/components/AnimalsDisplay.tsx
@@ -1,3 +1,27 @@
-export const AnimalsDisplay = () => {
-  return <>Animals display</>;
+import { IAnimal } from "../models/IAnimal";
+interface IAnimalsDisplayProps {
+  animals: IAnimal[];
+}
+export const AnimalsDisplay = (props: IAnimalsDisplayProps) => {
+  return (
+    <>
+      <section className="movies">
+        {props.animals.map((animal) => (
+          <div key={animal.id}>
+            <h3>{animal.name}</h3>
+            <h4>{animal.latinName}</h4>
+            <div>
+              <img
+                src={animal.imageUrl}
+                alt={animal.name}
+                width="200"
+                height="200"
+              />
+            </div>
+            <button className={animal.isFed ? "fed" : "notFed"}>Mata</button>
+          </div>
+        ))}
+      </section>
+    </>
+  );
 };
--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { AnimalsDisplay } from "../components/AnimalsDisplay";
 import { IAnimal } from "../models/IAnimal";
 import { useLoaderData } from "react-router-dom";
 
@@ -6,25 +7,5 @@ export const Animals = () => {
   const animals = useLoaderData() as IAnimal[];
   localStorage.setItem("animals", JSON.stringify(animals));
 
-  return (
-    <>
-      <section className="movies">
-        {animals.map((animal) => (
-          <div key={animal.id}>
-            <h3>{animal.name}</h3>
-            <h4>{animal.latinName}</h4>
-            <div>
-              <img
-                src={animal.imageUrl}
-                alt={animal.name}
-                width="200"
-                height="200"
-              />
-            </div>
-            <button className={animal.isFed ? "fed" : "notFed"}>Mata</button>
-          </div>
-        ))}
-      </section>
-    </>
-  );
+  return <AnimalsDisplay animals={animals}></AnimalsDisplay>;
 };
